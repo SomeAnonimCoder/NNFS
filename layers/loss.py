b86b6cb4ba2484d959e102d9d783b8466fa50a9f
Glubshev Artem <36304382+SomeAnonimCoder@users.noreply.github.com>
2022-07-16 08:40:39 +0300

    Reformat file

--- layers/loss.py
+++ layers/loss.py
@@ -1,12 +1,14 @@
 import numpy as np
 
+
 def mse(y_true, y):
     """Mean squred error
         :param y: actual output
         :param y_true: desired output
         :return: mse error
     """
-    return np.sum((y-y_true)**2)/len(y)
+    return np.sum((y - y_true) ** 2) / len(y)
+
 
 def mse_derivative(y_true, y):
     """Derivative of mean squred error, dE/dY
@@ -14,22 +16,24 @@ def mse_derivative(y_true, y):
         :param y_true: desired output
         :return mse derivative, dE/dY
     """
-    return 2*(y-y_true)/len(y)
+    return 2 * (y - y_true) / len(y)
+
 
 def mae(y_true, y):
     """Mean absolute error loss
     :param y: actual output
     :param y_true: desired output
     :return: mae error"""
-    return np.sum(np.abs(y-y_true))
+    return np.sum(np.abs(y - y_true))
+
 
 def mae_derivative(y_true, y):
     """Mean absolute error derivative
     :param y: actual output
     :param y_true: desired output
     :return: mae derivative"""
-    delta = y-y_true
-    return (delta>0).astype(float)-(delta<0).astype(float)
+    delta = y - y_true
+    return (delta > 0).astype(float) - (delta < 0).astype(float)
 
 
 def mape(y_true, y):
@@ -37,22 +41,24 @@ def mape(y_true, y):
     :param y: actual output
     :param y_true: desired output
     :return: mape error"""
-    return np.sum(np.abs((y-y_true)/y_true))
+    return np.sum(np.abs((y - y_true) / y_true))
+
 
 def mape_derivative(y_true, y):
     """Mean absolute percentage error derivative
     :param y: actual output
     :param y_true: desired output
     :return: mape derivative"""
-    delta = y-y_true
-    return (delta>0).astype(float)/np.abs(y_true)-(delta<0).astype(float)/np.abs(y_true)
+    delta = y - y_true
+    return (delta > 0).astype(float) / np.abs(y_true) - (delta < 0).astype(float) / np.abs(y_true)
 
 
 def _softmax(X):
     exps = np.exp(X - np.max(X))
     return exps / np.sum(exps)
 
-def cross_entropy(labels,logits):
+
+def cross_entropy(labels, logits):
     """
     Cross-entropy loss
     :param logits: output from neural net, shape is (num_examples,num_classes)
@@ -60,11 +66,12 @@ def cross_entropy(labels,logits):
     :return error
     """
     p = _softmax(logits)
-    log_likelihood = -np.log(p[0,labels])
+    log_likelihood = -np.log(p[0, labels])
     loss = np.sum(log_likelihood)
     return loss
 
-def cross_entropy_derivative(y,X):
+
+def cross_entropy_derivative(y, X):
     """
       Cross-entropy loss derivative
       :param logits: output from neural net, shape is (num_examples,num_classes)
@@ -72,6 +79,6 @@ def cross_entropy_derivative(y,X):
       :return error gradient
       """
     grad = _softmax(X)
-    grad[0,y] -= 1
+    grad[0, y] -= 1
     grad = grad
     return grad
