300617e41c8d3062b77fb5245e5a2853b1044ddd
Glubshev Artem <36304382+SomeAnonimCoder@users.noreply.github.com>
2022-07-16 05:37:52 +0300

    Add more loss functions

--- layers/loss.py
+++ layers/loss.py
@@ -15,3 +15,35 @@ def mse_derivative(y_true, y):
         :return mse derivative, dE/dY
     """
     return 2*(y-y_true)/len(y)
+
+def mae(y_true, y):
+    """Mean absolute error loss
+    :param y: actual output
+    :param y_true: desired output
+    :return: mae error"""
+    return np.sum(np.abs(y-y_true))
+
+def mae_derivative(y_true, y):
+    """Mean absolute error derivative
+    :param y: actual output
+    :param y_true: desired output
+    :return: mae derivative"""
+    delta = y-y_true
+    return (delta>0).astype(float)-(delta<0).astype(float)
+
+
+def mape(y_true, y):
+    """Mean absolute percentage error loss
+    :param y: actual output
+    :param y_true: desired output
+    :return: mape error"""
+    return np.sum(np.abs((y-y_true)/y_true))
+
+def mape_derivative(y_true, y):
+    """Mean absolute percentage error derivative
+    :param y: actual output
+    :param y_true: desired output
+    :return: mape derivative"""
+    delta = y-y_true
+    return (delta>0).astype(float)/np.abs(y_true)-(delta<0).astype(float)/np.abs(y_true)
+
