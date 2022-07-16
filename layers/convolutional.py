49eb7ca8f5e606aa99a0a9c722eff5cca519a34e
Glubshev Artem <36304382+SomeAnonimCoder@users.noreply.github.com>
2022-07-16 15:58:29 +0300

    Add multiple filters handling(still one channel)

--- layers/convolutional.py
+++ layers/convolutional.py
@@ -6,39 +6,23 @@ import scipy.signal as sp
 
 class Conv(BaseLayer):
 
-    def __init__(self, input_shape, kernel_size):
-        self.w = init_weights_tensor([kernel_size]*2)
-        self.b = init_weights(*input_shape)
-
-    def backward(self, out_error, lr):
-        res = sp.convolve(self.w[::-1], out_error, 'full')
-        we = sp.convolve(self.input, out_error,'valid')
-        self.b -= out_error * lr
-        self.w -= we * lr
-        return res
-
-    def fwd(self, input):
-        res = sp.convolve(input, self.w, 'same') + self.b
-        self.input = input
-        return res
-
-
-"""
-
     def __init__(self, input_shape, filters, kernel_size):
-        self.input_shape=input_shape
-        self.w = [init_weights_tensor([kernel_size]*(len(input_shape)-1)+[1]) for _ in range(filters)]
-        self.b = [init_weights(*input_shape[1:]) for _ in range(filters)]
+        self.filters = filters
+        self.w = [init_weights_tensor([kernel_size] * 2) for _ in range(filters)]
+        self.b = [init_weights(*input_shape) for _ in range(filters)]
 
     def backward(self, out_error, lr):
-        res = np.zeros(self.input_shape)
-        for filter,out in zip(self.w, out_error):
-            res += sp.convolve(filter[::-1], out, 'full')
+        res = []
+        for i in range(self.filters):
+            res.append(sp.convolve(self.w[i][::-1], out_error[i], 'full'))
+            we = sp.convolve(self.input, out_error[i], 'valid')
+            self.b[i] -= out_error[i] * lr
+            self.w[i] -= we * lr
         return res
 
     def fwd(self, input):
-        res=[]
-        for filter,bias in zip(self.w,self.b):
-            res.append(sp.convolve(input, filter, 'same')+bias)
+        self.input = input
+        res = []
+        for i in range(self.filters):
+            res.append(sp.convolve(input, self.w[i], 'same') + self.b[i])
         return res
-"""
